28eee06723e3e8e85564b062f7fecbc93a75a081
Huszár Fruzsina Anna (SZF_N_2025_2027) <huszar.fruzsina.anna2@diak.szamalk-szalezi.hu>
2026-05-13 14:39:38 +0200

    update: hibakezeles divvel

--- js/Service.js
+++ js/Service.js
@@ -1,25 +1,38 @@
 export default class Service {
   constructor() {}
 
-/*
- * 
- * !! Definíció
- *
- * Callback függvény
- * Egy aszinkron folyamatnak paraméterként átadott függvény, 
- * amely nem fut le azonnal,
- * hanem megvárja az adatok beérkezését, 
- * és csak a válasz megérkezése után hajtódik végre.
- *
- */
-  
+  /*
+   *
+   * !! Definíció
+   *
+   * Callback függvény
+   * Egy aszinkron folyamatnak paraméterként átadott függvény,
+   * amely nem fut le azonnal,
+   * hanem megvárja az adatok beérkezését,
+   * és csak a válasz megérkezése után hajtódik végre.
+   *
+   */
+
   getData(VEGPONT, callback) {
+    const errorDiv = document.querySelector(".error-message");
+
     fetch(VEGPONT)
-      .then((response) => response.json())
-      .then((data) => {
-        //console.log(data);
-        callback(data)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Hiba történt a termékek lekérése során.");
+        }
+        return response.json();
       })
-      .catch((error) => console.log(error));
+      .then((data) => {
+        if (errorDiv) errorDiv.style.display = "none";
+        callback(data);
+      })
+      .catch((error) => {
+        console.error("Fetch hiba:", error);
+        if (errorDiv) {
+          errorDiv.innerText = "Hiba történt a termékek lekérése során.";
+          errorDiv.style.display = "block";
+        }
+      });
   }
 }
